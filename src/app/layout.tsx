a4bb67eaf1f5bbb017adf8de4af2a97aa19c3bf4
Skeps <aliskepss@gmail.com>
2026-03-07 18:35:51 +0000

    Added Vercel analytics and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -31,6 +32,7 @@ export default function RootLayout({
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+        <Analytics/>
         {children}
         </body>
         </html>
