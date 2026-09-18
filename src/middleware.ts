eacfc93325fdcfd859613dd54842732a86446191
Mansoor Ali <aliskepss@gmail.com>
2026-09-18 22:45:18 +0100

    chore: remove Vercel endpoints from CSP

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,8 +20,8 @@ const csp = [
   "img-src 'self' blob: data:",
   "font-src 'self'",
   "style-src 'self' 'unsafe-inline'",
-  "script-src 'self' 'unsafe-inline' https://va.vercel-scripts.com",
-  "connect-src 'self' https://vitals.vercel-insights.com",
+  "script-src 'self' 'unsafe-inline'",
+  "connect-src 'self'",
   "upgrade-insecure-requests",
 ].join("; ");
 
